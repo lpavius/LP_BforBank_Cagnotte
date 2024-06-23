3fdb47dec8b9cc6c64a403c8b79e3aabe6d4308c
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-06-23 02:22:03 +0200

    first commit

---
+++ src/main/java/com/bforbank/cagnotte/Cagnotte1Application.java
@@ -0,0 +1,13 @@
+package com.bforbank.cagnotte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cagnotte1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cagnotte1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bforbank/cagnotte/Cagnotte1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bforbank.cagnotte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cagnotte1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
