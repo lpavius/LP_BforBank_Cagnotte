3977642aa5681fe391aacdfb28589fc166cef85c
Lucy Pavius <chun-cy@Mac-mini-de-Lucy.local>
2024-06-24 05:23:20 +0200

    test clientController check

--- src/test/java/com/bforbank/cagnotte/controller/ClientControllerTest.java
+++ src/test/java/com/bforbank/cagnotte/controller/ClientControllerTest.java
@@ -1,9 +1,41 @@
 package com.bforbank.cagnotte.controller;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.bforbank.cagnotte.service.ClientServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class ClientControllerTest {
+	
+	@Mock
+	private ClientServiceImpl clientService;
 
+	@InjectMocks
+	private ClientController clientController;
+	
+	private MockMvc mockMvc;
+	
+	@BeforeEach
+	void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(clientController).build();
+	}
+	
+	@Test
+	void testRecordCheckout() throws Exception {
+		mockMvc.perform(post("/api/checkout/record/1")).andExpect(status().isOk());
+		
+		verify(clientService, times(1)).checkoutClient(1L);
+	}
 }
